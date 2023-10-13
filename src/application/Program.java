f649d74c3834dd10206f31d163fa188888dcc09b
Renan Claude <renanclaude.rcg@gmail.com>
2023-10-13 20:52:16 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		Seller seller = new Seller("Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+
+		System.out.println(seller);
 
 	}
 
