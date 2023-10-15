5a10a25ac48dffff98c7cef3190411b8da10e72d
Renan Claude <renanclaude.rcg@gmail.com>
2023-10-15 19:58:07 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,16 @@ public class Program {
 			System.out.println(seller2);
 		}
 
-		System.out.println("\n==== TEST 4: seller insert ====");
-		Seller newSeller = new Seller("Greg", "greg@gmail.com", new Date(), 4000.00, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New Id = " + newSeller.getId());
+//		System.out.println("\n==== TEST 4: seller insert ====");
+//		Seller newSeller = new Seller("Greg", "greg@gmail.com", new Date(), 4000.00, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New Id = " + newSeller.getId());
+
+		System.out.println("\n==== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 
 	}
 
