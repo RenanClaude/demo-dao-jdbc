bd55336d368a63a925298d06e8aef112a7b725ff
Renan Claude <renanclaude.rcg@gmail.com>
2023-10-15 21:41:03 -0300

    Department findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,9 +13,16 @@ public class Program2 {
 	public static void main(String[] args) {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-		System.out.println("==== TEST 1: seller findById ====");
+		System.out.println("==== TEST 1: department findById ====");
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
+
+		System.out.println("\n==== TEST 2: department findAll ====");
+		List<Department> departmentList = new ArrayList<>();
+		departmentList = departmentDao.findAll();
+		for (Department department2 : departmentList) {
+			System.out.println(department2);
+		}
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -73,8 +76,30 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement("SELECT * FROM coursejdbc.department;");
+
+			rs = st.executeQuery();
+
+			List<Department> departmentList = new ArrayList<>();
+
+			while (rs.next()) {
+				Department department = instantiateDepartment(rs);
+
+				departmentList.add(department);
+			}
+
+			return departmentList;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
